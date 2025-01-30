c6c7bcde13f96ab038e660be43c0ea76446bbb8e
guiisaz <guilhermeazevedo320@gmail.com>
2025-01-30 18:31:56 -0300

    a few improvements in the guard i guess

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { TodoListGuard } from '../auth/guard/todo-list.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(TodoListGuard)
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
--- backend/src/auth/guard/todo-list.guard.ts
+++ backend/src/auth/guard/todo-list.guard.ts
@@ -1,30 +1,29 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { User } from '../../users/entities/user.entity';
 import { InjectModel } from '@nestjs/sequelize';
-import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class TodoListGuard {
       constructor(
         @InjectModel(User) private userModel: typeof User
       ) {}
     
-      async getToken(token: string){
-        const user = await this.userModel.findOne({where: { token }}) // Funçao pra pegar o Token e verificar se o Token existe ou n 
-        if (user){
+      async getToken(email?: string) {
+        const user = await this.userModel.findOne({where: { email }}) // Funçao pra pegar o Token e verificar se o Token existe ou n 
+        if (user?.token){
             return true
-        } else if (!user){
+        } else if (!user?.token){
             return false
         }
+        console.log(user)
       }
 
-    async canActivate( // Funcao que eu queria analisar o resultado do getToken e entao caso nao tivesse token (n fosse autenticado), retirasse as permissoes do usuário de mexer na todo-list, e caso tivesse token (caso fosse autenticado), pudesse mexer livremente na lista
-        context: ExecutionContext,
-    ): Promise<boolean | Observable<boolean>> {
+    async isPermissionGranted(){
         if (await this.getToken() === true) {
+            console.log("User authenthicated, permissions granted.") // Funcao que eu queria analisar o resultado do getToken e entao caso nao tivesse token (n fosse autenticado), retirasse as permissoes do usuário de mexer na todo-list, e caso tivesse token (caso fosse autenticado), pudesse mexer livremente na lista
             return true
         } else {
-            return false
+            console.log("User not authenthicated, permissions not granted.")
         }
     }
 }
